67bd157b0d22f3503f467440fc8510afcb01bf9e
Marcelo Pereira <marcelo.pereira@flexdoc.com.br>
2023-09-08 14:00:49 -0300

    ajustes

--- src/app/scanner/services/opencv.service.ts
+++ src/app/scanner/services/opencv.service.ts
@@ -9,17 +9,12 @@ export class OpencvService {
 
   constructor() { }
 
-  loadOpenCV(): Promise<void> {
-    return new Promise(async (resolve, reject) => {
-      // opencv inicializado
-      // cv.onRuntimeInitialized = () => resolve();
-
+  loadOpenCV() {
+    return new Promise<void>((resolve, reject) => {
       // verifica se opencv já foi carregado
       if (this.isOpenCvLoaded) {
         resolve();
-
       } else {
-
         // cria elemento script para carregar opencv
         const script = document.createElement('script');
         script.src = 'https://docs.opencv.org/4.8.0/opencv.js';
@@ -32,7 +27,7 @@ export class OpencvService {
         script.onerror = (err) => {
           this.isOpenCvLoaded = false;
           console.error('OPENCV ERROR', err);
-          reject();
+          reject(err);
         };
         // adiciona script ao head
         document.head.appendChild(script);
